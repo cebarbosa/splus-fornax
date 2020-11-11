2bc38356c602d2821f84a04823045723dea9dbf3
cebarbosa <carlos.barbosa@usp.br>
2020-11-11 18:01:52 -0300

    Making stamps for FCC and LSBs again with new data structure for calibration.

--- make_stamps.py
+++ make_stamps.py
@@ -63,6 +63,11 @@ def make_stamps_jype(names, coords, sizes, outdir=None, redo=False,
 
 
     """
+    names = np.atleast_1d(names)
+    sizes = np.atleast_1d(sizes)
+    if len(sizes) == 1:
+        sizes = np.full(len(names), sizes[0])
+    sizes = sizes.astype(np.int)
     img_types = ["swp", "swpweight"] if img_types is None else img_types
     outdir = os.getcwd() if outdir is None else outdir
     tiles_dir = "/mnt/jype/MainSurvey/tiles/T01"
@@ -135,17 +140,27 @@ def make_stamps_jype(names, coords, sizes, outdir=None, redo=False,
 def make_stamps_fcc():
     """Produces stamps for sample of galaxies in the FCC (Ferguson+ 1990)."""
     fcc_cat = Table.read(os.path.join(context.tables_dir, "FCC_sizes.fits"))
-    fcc_coords = SkyCoord(fcc_cat["RA"].data, fcc_cat["DEC"].data,
-                          unit=(u.degree, u.degree))
-    fcc_names = [ _ for _ in fcc_cat["Object"]]
-    outdir = os.path.join(context.data_dir, "cutouts")
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    sizes = 1 + 1.2 * fcc_cat["Major Axis"].data / context.ps.value
+    fcc_names = np.array([ _ for _ in fcc_cat["Object"]])
+    ras =  np.array([ _ for _ in fcc_cat["RA"]])
+    decs = np.array([ _ for _ in fcc_cat["DEC"]])
+    wdir = os.path.join(context.data_dir, "FCC")
+    outdir = os.path.join(wdir, "cutouts")
+    for _dir in [wdir, outdir]:
+        if not os.path.exists(_dir):
+            os.mkdir(_dir)
+    sizes = np.array(1 + 1.2 * fcc_cat["Major Axis"].data /
+                     context.ps.value).astype(np.int)
     sizes = np.clip(sizes.astype(np.int), 256, np.infty)
+    # Duplicating large galaxies
+    idx = np.where(sizes > 256)
+    fcc_names = np.append(fcc_names, fcc_names[idx])
+    ras = np.append(ras, ras[idx])
+    decs = np.append(decs, decs[idx])
+    sizes = np.append(np.full(len(fcc_cat), 256), sizes[idx])
+    fcc_coords = SkyCoord(ras, decs, unit=(u.degree, u.degree))
     make_stamps_jype(fcc_names, fcc_coords, sizes, outdir=outdir)
 
-def make_stamps_lsb_idr3():
+def make_stamps_FDS_lsb():
     catname = os.path.join(context.tables_dir, "Venhola2017_FDS_LSB.dat")
     with open(catname) as f:
         data = f.readlines()
@@ -154,7 +169,7 @@ def make_stamps_lsb_idr3():
     ras = np.array([float(_[22:30]) for _ in data]) * u.degree
     decs = np.array([float(_[31:38])  for _ in data]) * u.degree
     res = np.array([float(_[74:80])  for _ in data])
-    sizes = np.floor(1 + 2 * 6 * res / context.ps.value)
+    sizes = (np.floor(1 + 2 * 6 * res / context.ps.value)).astype(np.int)
     coords = SkyCoord(ras, decs)
     wdir = os.path.join(context.data_dir, "FDS_LSB")
     if not os.path.exists(wdir):
@@ -164,7 +179,40 @@ def make_stamps_lsb_idr3():
         os.mkdir(outdir)
     make_stamps_jype(names, coords, sizes, outdir=outdir, redo=True)
 
+def make_stamps_11HUGS():
+    catname = os.path.join(context.tables_dir, "kennicutt2008.fits")
+    outdir = os.path.join(context.data_dir, "stamps_11HUGS")
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    sample = Table.read(catname)
+    names = [_ for _ in sample["Name"]]
+    coords = SkyCoord(sample["RAJ2000"], sample["DEJ2000"], unit=u.degree)
+    fov = 14. * u.arcmin
+    sizes = fov.to(u.arcsec).value / context.ps.value
+    make_stamps_jype(names, coords, sizes, outdir=outdir)
+
+def make_stamps_FDS_dwarfs_idr3():
+    catname = os.path.join(context.tables_dir, "Venhola2018_FDS_dwarfs.dat")
+    with open(catname) as f:
+        data = f.readlines()
+    data = data[152:] # Removing header
+    names = np.array([_[0:14].strip() for _ in data])
+    ras = np.array([float(_[16:23]) for _ in data]) * u.degree
+    decs = np.array([float(_[24:32])  for _ in data]) * u.degree
+    res = np.array([float(_[126:135])  for _ in data]) * u.arcsec
+    sizes = (np.floor(1 * u.pix + 2 * 3 * res / context.ps)).astype(
+        np.int).value
+    coords = SkyCoord(ras, decs)
+    wdir = os.path.join(context.data_dir, "FDS_dwarfs")
+    if not os.path.exists(wdir):
+        os.mkdir(wdir)
+    outdir = os.path.join(wdir, "cutouts")
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    make_stamps_jype(names, coords, sizes, outdir=outdir, redo=True)
 
 if __name__ == "__main__":
-    # make_stamps_fcc()
-    make_stamps_lsb_idr3()
+    make_stamps_fcc()
+    make_stamps_FDS_lsb()
+    # make_stamps_11HUGS()
+    # make_stamps_FDS_dwarfs_idr3()
