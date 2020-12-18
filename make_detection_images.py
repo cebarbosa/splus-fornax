de15cef419d5de061b8a9dff8133815bcaf3ab0b
cebarbosa <kadu.barbosa@gmail.com>
2020-12-18 17:24:10 -0300

    Updated figure to use non-calibrated images.

--- make_detection_images.py
+++ make_detection_images.py
@@ -34,36 +34,80 @@ def make_detection_img(filenames, outfile, bands=None):
     hdul.writeto(outfile, overwrite=True)
     return
 
-def make_detection_images(survey, overwrite=False):
+def make_detection_images(survey, overwrite=False, maxdim=1000):
     survey_dir =  os.path.join(context.data_dir, survey)
-    wdir = os.path.join(survey_dir, "scubes")
+    wdir = os.path.join(survey_dir, "cutouts")
     outdir = os.path.join(survey_dir, "detection")
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     sample = os.listdir(wdir)
-    for scube in tqdm(sample, desc="Processing {}".format(survey)):
-        galid = "_".join(scube.split("_")[:-1])
-        output = os.path.join(outdir, scube.replace(".fits", ".png"))
-        if os.path.exists(output) and not overwrite:
-            continue
-        data = fits.getdata(os.path.join(wdir, scube), ext=0)
-        error = fits.getdata(os.path.join(wdir, scube), ext=1)
-        mask = np.where(data == 0)
-        data[mask] = np.nan
-        detection = np.nansum(data, axis=(0,))
-        deterr = np.sqrt(np.nansum(error**2, axis=(0,)))
-        sigma = detection / deterr
-        plt.imshow(gaussian_filter(sigma, 2), origin="lower", vmax=1, vmin=-1,
-                   cmap="Spectral")
-        plt.colorbar()
-        plt.title(galid.replace("_", "\_"))
-        plt.tight_layout()
-        plt.savefig(output, dpi=250)
-        plt.clf()
+    for galaxy in tqdm(sample, desc="Processing {}".format(survey)):
+        galdir = os.path.join(wdir, galaxy)
+        obsfields = set([_.split("_")[-4] for _ in os.listdir(galdir)])
+        dims = set([_.split("_")[-2] for _ in os.listdir(galdir)])
+        for dim in dims:
+            xymax = np.max([float(_) for _ in dim.split("x")])
+            if xymax >= maxdim:
+                continue
+            for obsfield in obsfields:
+                output = os.path.join(outdir, "{}_{}_{}_detect.fits".format(
+                                      galaxy, obsfield, dim))
+                outpng = output.replace(".fits", ".png")
+                if os.path.exists(outpng) and not overwrite:
+                    continue
+                imgs = [os.path.join(galdir, _) for _ in os.listdir(galdir) if
+                        dim in _ and obsfield in _ and _.endswith("swp.fits")]
+                imgs = [_ for _ in imgs if os.path.exists(_) and
+                        os.path.exists(_.replace("swp", "swpweight"))]
+                headers = [fits.getheader(img, ext=1) for img in imgs]
+                gain = np.array([h["GAIN"] for h in headers])
+                wimgs = [_.replace("swp", "swpweight") for _ in imgs]
+                data = np.array([fits.getdata(_, ext=0) for _ in imgs])
+                weights = np.array([fits.getdata(os.path.join(galdir, _), ext=0)
+                                 for _ in wimgs])
+                error = 1 / weights + np.clip(data, 0, np.infty) / gain[:,
+                                                                     None, None]
+                mask = np.where(data == 0)
+                detection = np.nansum(data, axis=(0,))
+                deterr = np.sqrt(np.nansum(error ** 2, axis=(0,)))
+                sigma = detection / deterr
+                title = "galaxy: {}; field: {}".format(
+                         galaxy.replace("_", " "), obsfield)
+                ydim, xdim = sigma.shape
+                extent = np.array([-0.5 * xdim, 0.5 * xdim, -0.5 * ydim,
+                                  0.5 * ydim]) * context.ps.value
+                plt.imshow(gaussian_filter(sigma, 1), origin="lower",
+                           cmap="viridis", vmax=5, vmin=0, extent=extent)
+                plt.colorbar()
+                plt.title(title)
+                plt.xlabel("$\Delta$ X (arcsec)")
+                plt.ylabel("$\Delta$ Y (arcsec)")
+                plt.savefig(outpng, dpi=250)
+                plt.clf()
+
+
+        # output = os.path.join(outdir, galaxy.replace(".fits", ".png"))
+        # if os.path.exists(output) and not overwrite:
+        #     continue
+        # data = fits.getdata(os.path.join(wdir, galaxy), ext=0)
+        # error = fits.getdata(os.path.join(wdir, galaxy), ext=1)
+        #
+        # data[mask] = np.nan
+        # detection = np.nansum(data, axis=(0,))
+        # deterr = np.sqrt(np.nansum(error**2, axis=(0,)))
+        # sigma = detection / deterr
+        # plt.imshow(gaussian_filter(sigma, 2), origin="lower", vmax=1, vmin=-1,
+        #            cmap="Spectral")
+        # plt.colorbar()
+        # plt.title(galid.replace("_", "\_"))
+        # plt.tight_layout()
+        # plt.savefig(output, dpi=250)
+        # plt.clf()
 
 if __name__ == "__main__":
     np.seterr(divide='ignore', invalid='ignore')
-    surveys = ["smudges2", "patricia", "FDS_dwarfs", "FDS_LSB", "FCC", "11HUGS"]
-    surveys = ["jellyfish"]
+    surveys = ["FDS_LSB"]
+    # surveys = ["smudges2", "patricia", "FDS_dwarfs", "FCC", \
+    #           "11HUGS", "jellyfish"]
     for survey in surveys:
         make_detection_images(survey, overwrite=False)
